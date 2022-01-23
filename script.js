50329ebf9e31bfab1527d17d7987cdba37eaf3fe
avagagner <46656654+avagagner@users.noreply.github.com>
2022-01-23 13:40:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-var map = L.map('mapid').setView([38.6, -90.1], 10);
-
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
+//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
+//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
+//https://wiki.openstreetmap.org/wiki/Tile_servers
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(map);
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(mymap);
 
-var marker = L.marker([38.6, -90.1]).addTo(map);
-var marker = L.marker([38.627, -90.199]).addTo(map);
+var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
 
-var circle = L.circle([38.627, -90.199], {
-    color: 'green',
-    fillColor: '#097969',
-    fillOpacity: 0.5,
-    radius: 3000
-}).addTo(map);
+var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
 
-var polygon = L.polygon([
-    [38.6, -90.199],
-    [38.7, -90.25],
-    [38.6, -90.1]
-]).addTo(map);
+var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
 
-/*
-var popup = L.popup()
-    .setLatLng([38.6, -90.1])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
- */
+var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
 
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-}
 
-map.on('click', onMapClick);
+
+marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
+marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
+marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
+marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
+marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
